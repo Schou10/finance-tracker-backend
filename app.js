70f4db4646849dbac25f9e6466c42727dbbf82df
$chouty <aschou101229@gmail.com>
2025-01-28 17:51:18 -0600

    Fixed cors options

--- app.js
+++ app.js
@@ -22,8 +22,11 @@ mongoose.connection.on('error', err => {
 });
 
 app.use(express.json());
-app.use(cors("*"));
-
+app.use(cors({
+    origin: 'https://finance-tracker.zanity.net', // Adjust to your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],     // Include allowed methods
+    credentials: true    // Include this if you need to send cookies or authentication info
+  }));
 
 // app.use(bodyParser.json());
 
