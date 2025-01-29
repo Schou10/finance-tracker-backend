ff4c65302ea88ac7b63ccf2a1b0130c076af885d
$chouty <aschou101229@gmail.com>
2025-01-29 10:24:25 -0600

    Testing env

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-console.log(process.env.ENCRYPTION_KEY);
+console.log(process.env);
 
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY.padEnd(16, '0').slice(0, 16);
 const IV_LENGTH = 16;
